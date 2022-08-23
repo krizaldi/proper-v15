8377921c8414551c956ac4fb0bdd6408830c4214
clopez-ola <clopez@grupoola.net>
2022-08-23 16:08:24 -0500

    saddas

--- partner_routes_map_oin/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-#
-# Copyright 2020 Odoo IT now <http://www.odooitnow.com/>
-# See LICENSE file for full copyright and licensing details.
-#
-##############################################################################
-
-from . import wizard
